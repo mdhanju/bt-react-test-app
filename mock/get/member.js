3c2d67cc4c521e101231f15314c1057caf166688
Sameer Tandon <sameer.tandon@kp.org>
2020-08-17 15:51:56 -0700

    Added new Get for meber

---
+++ mock/get/member.js
@@ -0,0 +1,11 @@
+module.exports = {
+    path: '/clinicians?searchString=sam',
+    delay: 2000,
+    template: {
+        "id": "1211",
+        "page": "Profile",
+        "Name": "User",
+        "id": "123456",
+        "url": "http://localhost:4000/profile"
+    }
+}
